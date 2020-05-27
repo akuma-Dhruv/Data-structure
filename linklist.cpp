6d5b7b783bd6f3a2569fcbacd944b2edc000a7db
akuma-dhruv <dhruvgarg79@gmail.com>
2020-05-27 21:42:27 +0530

    linklist

---
+++ linklist.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+class Node{
+public:
+int data;
+Node *next;
+node(int d)
+{
+    data=d;
+    next =NULL;
+}
+};
+
+
+
+int main(void)
+{
+ Node n1(2);
+ Node n2(4);
+ Node n3(6);
+ Node n4(8);
+ Node *head=&n1;
+
+}
