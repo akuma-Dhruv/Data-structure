566a36bd443d73a043ffb333decd47113a65b2fe
akuma-dhruv <dhruvgarg79@gmail.com>
2020-05-29 17:22:31 +0530

    linked list

--- linklist.cpp
+++ linklist.cpp
@@ -4,13 +4,19 @@ class Node{
 public:
 int data;
 Node *next;
-node(int d)
+Node(int d)
 {
     data=d;
     next =NULL;
 }
 };
-
+void print(Node *head) {
+Node *temp = head;
+		while(temp != NULL) {
+		cout << temp -> data << " ";
+		temp = temp -> next;
+	}
+}
 
 
 int main(void)
@@ -20,5 +26,9 @@ int main(void)
  Node n3(6);
  Node n4(8);
  Node *head=&n1;
+	n1.next = &n2;
+	n2.next = &n3;
+	n3.next = &n4;
+	print(head);
 
 }
