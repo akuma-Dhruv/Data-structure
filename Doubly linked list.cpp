c32539ab333834819de2fdf99585c9d315faedf6
akuma-dhruv <dhruvgarg79@gmail.com>
2020-06-08 21:18:58 +0530

    Input & print function

--- Doubly linked list.cpp
+++ Doubly linked list.cpp
@@ -13,3 +13,52 @@ Node(int d)
 	prev= NULL;
 }
 };
+Node* takeInput()
+{
+	int data;
+	cin>>data;
+	Node *head =NULL;
+	Node *tail= NULL;
+	Node *pre=NULL;
+	while(data!=-1)
+	{
+	    Node *newNode= new Node(data);
+		if(head==NULL)
+		{
+		    head=newNode;
+            tail=newNode;
+        }
+		else
+        {	tail->next=newNode;
+			pre=tail;
+			tail=tail->next;
+			tail->prev = pre;
+		}
+		cin>>data;
+	}
+	head->prev=NULL;
+
+	return head;
+}
+void print(Node *head)
+{
+        Node *temp = head;
+		while(temp->next!=NULL)
+    {
+		//cout << temp -> data << " ";
+		temp = temp -> next;
+	}
+	while(temp!=NULL)
+    {
+		cout << temp -> data << " ";
+		temp = temp -> prev;
+	}
+
+
+}
+int main()
+{
+	Node *head = takeInput();
+	print(head);
+}
+
