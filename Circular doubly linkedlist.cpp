a5965ac2f8fc6a70f58348c6b9795c2f874b9e4a
akuma-dhruv <dhruvgarg79@gmail.com>
2020-06-09 08:23:53 +0530

    Circular doubly linked list

---
+++ Circular doubly linkedlist.cpp
@@ -0,0 +1,61 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+public:
+int data;
+Node *next;
+Node *prev;
+Node(int d)
+{
+    data=d;
+    next =NULL;
+	prev= NULL;
+}
+};
+Node* takeInput()
+{
+	int data;
+	cin>>data;
+	Node *head =NULL;
+	Node *tail= NULL;
+	Node *pre=NULL;
+	while(data!=-1)
+	{
+	    Node *newNode= new Node(data);
+		if(head==NULL)
+		{
+		    head=newNode;
+            tail=newNode;
+        }
+		else
+        {	tail->next=newNode;
+			pre=tail;
+			tail=tail->next;
+			tail->prev = pre;
+		}
+		cin>>data;
+	}
+	tail->next =head;
+	head->prev=tail;
+
+	return head;
+}
+void print(Node *head)
+{
+        Node *temp = head;
+		while(temp->next!=head)
+    {
+		cout << temp -> data << " ";
+		temp = temp -> next;
+	}
+	cout<<temp->data;           //for the very last element
+}
+
+
+int main()
+{
+	Node *head = takeInput();
+	print(head);
+}
+
