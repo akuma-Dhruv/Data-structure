c807d45c77e3ca32593f109dc5d502d04e068aca
akuma-dhruv <dhruvgarg79@gmail.com>
2020-06-10 22:25:44 +0530

    Stack using linked list

---
+++ stack/stack linked list.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+using namespace std;
+
+class Node {
+	public :
+		int data;
+		Node *prev;
+
+		Node(int data) {
+			this -> data = data;
+			prev = NULL;
+		}
+
+		~Node() {
+			delete prev;
+		}
+};
+
+class Stack {
+	Node *head;
+	int sizze;
+
+	public :
+
+	Stack() {
+		head=NULL;
+		sizze =0;
+
+	}
+
+	int getSize() {
+		return sizze;
+	}
+
+	bool isEmpty() {
+	return sizze == 0;
+
+	}
+
+	void push(int input) {
+		sizze++;
+		Node *newNode;
+		newNode = new Node(input);
+		newNode->prev=head;
+		head=newNode;
+
+	}
+
+	int pop() {
+		Node * a;
+
+		a= head;
+		int d=a->data;
+		head=head->prev;
+		sizze--;
+		delete a;
+		return d;
+	}
+
+	int top() {
+		return head->data;
+	}
+
+};
+int main()
+{
+Stack L;
+L.push(4);
+L.push(6);
+L.push(5);
+L.push(4);
+L.push(9);
+cout<<L.getSize()<<endl;
+cout<<L.top()<<endl;
+cout<<L.pop()<<endl;
+cout<<L.pop()<<endl;
+cout<<L.pop()<<endl;
+
+cout<<L.isEmpty()<<endl;
+
+
+}
