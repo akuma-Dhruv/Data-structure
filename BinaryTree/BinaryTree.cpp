e80f220bbfc24dd6c39fd0271c006e38439a9a5f
akuma-dhruv <dhruvgarg79@gmail.com>
2020-06-20 12:57:44 +0530

    print Binary tree

--- BinaryTree/BinaryTree.cpp
+++ BinaryTree/BinaryTree.cpp
@@ -2,16 +2,34 @@
  #include"BinaryTreeClass.h"
  using namespace std;
 #define N NULL
+#define L left
+#define R right
+#define D data
+
+
 void print(BinaryTree<int>* root)
 {
     if(root==N)
         return;
-    cout<<root->data<<endl;
-print(root->left);
-print(root->right);
+cout<<root->D<<":";
+if (root->L!=N) {
+		cout << "L " << root->L->D<<" ";
+	}
+
+	if (root->R!=N) {
+		cout << "R " << root->R->D;
+	}
+	cout<<endl;
+print(root->L);
+print(root->R);
 
 }
-void main()
+int main()
 {
-
+BinaryTree<int>* root = new BinaryTree<int>(1);
+	BinaryTree<int>* node1 = new BinaryTree<int>(2);
+	BinaryTree<int>* node2 = new BinaryTree<int>(3);
+	root->left = node1;
+	root->right = node2;
+	print(root);
 }
