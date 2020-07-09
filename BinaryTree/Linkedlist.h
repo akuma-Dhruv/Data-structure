16bfa2c81f6d41587658e7e14bc487b36aa51f19
akuma-dhruv <dhruvgarg79@gmail.com>
2020-07-09 09:04:42 +0530

    in print function added

--- BinaryTree/Linkedlist.h
+++ BinaryTree/Linkedlist.h
@@ -8,4 +8,14 @@ Node(int d)
     data=d;
     next =NULL;
 }
+
+void printLL(Node *head)
+{
+        Node *temp = head;
+		while(temp != NULL)
+    {
+		std::cout << temp -> data << " ";
+		temp = temp -> next;
+	}
+}
 };
