dc203a67dcc7c997d4e5107f9fcdbc39da8168eb
akuma-dhruv <dhruvgarg79@gmail.com>
2020-07-16 21:55:32 +0530

    is cousin final

--- BinaryTree/isCousin.cpp
+++ BinaryTree/isCousin.cpp
@@ -86,13 +86,21 @@ BinaryTree<int>* getparent(BinaryTree<int>* root,int e)
 
 
 }
+bool iscousin(BT* root,int p,int q)
+{
+    int pd=getdept(root,p);
+    int qd=getdept(root,q);
+    BT* pp= getparent(root,p);
+    BT* qp= getparent(root,q);
+    return ((pd==qd)&&(pp!=qp));
+}
 
 ///1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 -1 24 -1 26 27 -1 28 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 29 -1 -1 -1 -1 -1 30 -1 -1 -1 -1 -1 -1 -1 -1 -1
 int main()
 {
     BinaryTree<int>*root=takeInputLevelWise();
-    BT* g=getparent(root,30);
-    cout<<g->D;
+    //BT* g=getparent(root,30);
+    cout<<iscousin(root,30,29);
 
 
 }
