6429f4135095fd28bfa05c1bbcf06a81671fd4a5
akuma-dhruv <dhruvgarg79@gmail.com>
2020-07-28 22:34:59 +0530

    priority queue class

---
+++ PriorityQueue/PQueueClass.h
@@ -0,0 +1,25 @@
+#include<vector>
+using namespace std;
+
+class PQueue
+{
+vector <int> PQ;
+    public:
+PQueue(){
+
+}
+bool isEmpty()
+{
+return PQ.size==0;
+}
+int getsize()
+{
+return PQ.size;
+}
+int getMin()
+{if(isEmpty())
+return 0;
+else
+return PQ[0];
+}
+}
