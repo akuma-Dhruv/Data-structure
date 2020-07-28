cca2765a8832871a71ca18c052654cae7bbe4e25
akuma-dhruv <dhruvgarg79@gmail.com>
2020-07-28 23:05:00 +0530

    insert function test

--- PriorityQueue/PQ.cpp
+++ PriorityQueue/PQ.cpp
@@ -3,8 +3,14 @@
 using namespace std;
 int main()
 {PQueue q;
-cout<<q.getsize();
-cout<<q.isEmpty();
-cout<<q.getMin();
+q.Insert(990);
+q.Insert(20);
+q.Insert(30);
+q.Insert(40);
+q.Insert(50);
+q.Insert(60);
+cout<<q.getsize()<<endl;
+cout<<q.isEmpty()<<endl;
+cout<<q.getMin()<<endl;
 
 }
