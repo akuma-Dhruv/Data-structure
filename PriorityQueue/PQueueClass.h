b1d622c0378a2b2c7f59f48cb2a3fcbd379ad7cf
akuma-dhruv <dhruvgarg79@gmail.com>
2020-07-28 23:05:23 +0530

    insert function

--- PriorityQueue/PQueueClass.h
+++ PriorityQueue/PQueueClass.h
@@ -22,4 +22,22 @@ return 0;
 else
 return PQ[0];
 }
+void Insert(int elements)
+{
+    PQ.push_back(elements);
+    int childIndex = PQ.size()-1;
+    while(childIndex>0)
+    {   int parentIndex = (childIndex-1)/2;
+        if(PQ[childIndex]<PQ[parentIndex])
+        {
+            //swap
+            int temp = PQ[childIndex];
+            PQ[childIndex]=PQ[parentIndex];
+            PQ[parentIndex]= temp;
+        }
+        else
+            break;
+        childIndex =parentIndex;
+    }
+}
 };
