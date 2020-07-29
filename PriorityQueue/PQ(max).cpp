333c4097407a28a20bc49de20f4997ebbc54a475
akuma-dhruv <dhruvgarg79@gmail.com>
2020-07-29 20:25:39 +0530

    max priority queue test

---
+++ PriorityQueue/PQ(max).cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include"PQueueClass(max).h"
+using namespace std;
+int main()
+{PQueue q;
+q.Insert(990);
+q.Insert(20);
+q.Insert(30);
+q.Insert(40);
+q.Insert(50);
+q.Insert(60);
+cout<<q.getsize()<<endl;
+cout<<q.isEmpty()<<endl;
+cout<<q.getMax()<<endl;
+//cout<<q.removeMin();
+
+}
