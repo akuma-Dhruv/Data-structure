68fd33cbf90bde15eb2bd4b9ae7a7682b0a253ef
akuma-dhruv <dhruvgarg79@gmail.com>
2020-07-31 23:50:54 +0530

    minimum priority queue inbuilt

---
+++ PriorityQueue/PQmin(inbuilt).cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+int main()
+{
+priority_queue<int,vector<int>,greater<int>> pq;
+pq.push(44);
+pq.push(7);
+pq.push(66);
+pq.push(21);
+pq.push(12);
+pq.push(8);
+pq.push(89);
+pq.push(11);
+
+cout<<"Size: "<<pq.size()<<endl;
+cout<<"Top: " <<pq.top()<<endl;
+while(!pq.empty())
+{
+    cout<<pq.top()<<" ";
+    pq.pop();
+}
+}
