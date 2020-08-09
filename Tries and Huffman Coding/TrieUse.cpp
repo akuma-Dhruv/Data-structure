c56a11c1ef514e628e3b2cf83dd58cfecfd4f3dd
akuma-dhruv <dhruvgarg79@gmail.com>
2020-08-09 20:52:52 +0530

    remove function test

--- Tries and Huffman Coding/TrieUse.cpp
+++ Tries and Huffman Coding/TrieUse.cpp
@@ -5,9 +5,14 @@ int main()
 {
     Trie t;
     t.insertWord("dot");
+    t.insertWord("boat");
+    t.insertWord("born");
     t.insertWord("do");
     t.insertWord("bot");
 
-    cout<<t.searchWord("dot");
+    cout<<t.searchWord("do")<<endl;
+	t.removeWord("born");
+    cout<<t.searchWord("boat");
+
 
 }
